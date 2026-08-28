6b07b100292cc351490b332804438e2f471dbb48
Arthur Piloian <32160804+apiloian@users.noreply.github.com>
2026-08-28 11:05:21 -0700

    Add: top-k-elements-in-list - submission-0

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-0.ts
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} k
+     * @return {number[]}
+     */
+    topKFrequent(nums: number[], k: number): number[] {
+        const counts = {} as Record<string, number>;
+
+        for(const n of nums) {
+            counts[n] = (counts[n] || 0) + 1;
+        }
+
+        const arr = new Array(nums.length);
+
+        for(const [n, count] of Object.entries(counts)) {
+            if(!arr[count]) {
+                arr[count] = [];
+            }
+            arr[count].push(n);
+        }
+
+        const res = [];
+
+        for(let i = arr.length - 1; i >= 0; i--) {
+            while(arr[i] && arr[i].length && (res.length < k)) {
+                const el = arr[i].pop();
+                res.push(el);
+            }
+
+            if((res.length - 1) === k) {
+                break
+            }
+        }
+
+        return res;
+    }
+}
