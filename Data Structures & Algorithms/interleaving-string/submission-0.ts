b573a7d62d183fa5ab65edfe118506583f1d1bd5
Arthur Piloian <32160804+apiloian@users.noreply.github.com>
2026-09-11 17:20:33 -0700

    Add: interleaving-string - submission-0

---
+++ Data Structures & Algorithms/interleaving-string/submission-0.ts
@@ -0,0 +1,30 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @param {string} s3
+     * @return {boolean}
+     */
+    isInterleave(s1: string, s2: string, s3: string): boolean {
+        if(s1.length + s2.length !== s3.length) {
+            return false
+        }
+
+        const dp = Array.from({ length: s1.length + 1}, () => new Array(s2.length + 1).fill(false));
+        dp[s1.length][s2.length] = true;
+
+        for(let i = s1.length; i >= 0; i--) {
+            for(let j = s2.length; j >= 0; j--) {
+
+                if(i < s1.length && s1[i] === s3[i + j] && dp[i + 1][j]) {
+                    dp[i][j] = true
+                }
+
+                if(j < s2.length && s2[j] === s3[i + j] && dp[i][j + 1]) {
+                    dp[i][j] = true
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
