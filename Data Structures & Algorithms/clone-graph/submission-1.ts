aadd84ecc3119626e0caa5a96ff6bd227e202c2b
Arthur Piloian <32160804+apiloian@users.noreply.github.com>
2026-09-13 21:47:25 -0700

    Add: clone-graph - submission-1

---
+++ Data Structures & Algorithms/clone-graph/submission-1.ts
@@ -0,0 +1,42 @@
+
+// Definition for a Node.
+class RNode {
+    val: number
+    neighbors: any[]
+    constructor(val = 0, neighbors = []) {
+      this.val = val;
+      this.neighbors = neighbors;
+    }
+}
+
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node: RNode | null): RNode {
+        const clonned = {};
+        function dfs(n: RNode) {
+            if(!n) {
+                return null
+            }
+
+            if(clonned[n.val]) {
+                return clonned[n.val]
+            }
+
+            const clone = new RNode(n.val);
+            clonned[n.val] = clone;
+
+            for(const neighbor of n.neighbors) {
+                const clonNedneighbor = dfs(neighbor);
+                clone.neighbors.push(clonNedneighbor);
+            }
+
+            return clone
+        }
+
+        return dfs(node)
+    }
+}
