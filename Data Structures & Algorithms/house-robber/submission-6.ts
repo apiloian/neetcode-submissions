8618de69c909b6f196d7ad52f60c0119286792b2
Arthur Piloian <32160804+apiloian@users.noreply.github.com>
2026-09-17 08:14:49 -0700

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.ts
@@ -0,0 +1,37 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    // rob(nums: number[]): number {
+    //     if(nums.length < 2) {
+    //         return nums[0];
+    //     }
+
+    //     const dp = new Array(nums.length);
+    //     dp[0] = nums[0];
+    //     dp[1] = Math.max(nums[0], nums[1]);
+
+    //     for(let i = 2; i < dp.length; i++) {
+    //         dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+    //     }
+
+    //     return dp[nums.length - 1];
+    // }
+
+    rob(nums: number[]) {
+        if(nums.length < 2) {
+            return nums[0];
+        }
+        const dp = new Array(nums.length);
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1])
+
+        for(let i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i] + dp[i - 2]);
+        }
+
+        return dp[dp.length - 1]
+
+    }
+}
